1326e50c152d36ce8283ae50913dec137dee26dd
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-13 01:45:00 +0600

    scope,js added

---
+++ scope.js
@@ -0,0 +1,22 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "funky";
+        mood = "sad";
+
+        // console.log(mood);
+    }
+
+    console.log(day);
+    let day = "Wednesday"
+    return result;
+}
+
+const output = sum(3, 7);
+// console.log(bonus);
+// console.log(output);

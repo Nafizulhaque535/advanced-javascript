f5d87332b850841fe9b68eb90ecd9f95c0b417aa
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-12 22:58:42 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,15 @@
+// false
+// 0
+// ""
+// undefined
+// null
+//NaN               [Falsy]
+
+// '0', ' '
+let name = 12;
+if(name || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

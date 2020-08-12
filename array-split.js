84adf3eab337ab132e4f829012c9e882d8354602
Nafizulhaque535 <codeglobal35@gmail.com>
2020-08-13 02:19:08 +0600

    split, splice added

---
+++ array-split.js
@@ -0,0 +1,11 @@
+const nums = [1,2,3,4,5,6,7,8];
+
+const part = nums.slice(2, 4);
+const removed = nums.splice(2, 3, 77, 88);
+
+console.log(part);
+// console.log(removed);
+// console.log(nums);
+
+const together = nums.join(" , ");
+console.log(together);
